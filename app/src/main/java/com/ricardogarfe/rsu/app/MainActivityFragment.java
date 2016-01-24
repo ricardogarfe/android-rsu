df3f097eda7ac0adfdfd33f3b147b8e58d8de815
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-01-24 17:14:54 +0100

    Show fake data on a ListView

--- app/src/main/java/com/ricardogarfe/rsu/app/MainActivityFragment.java
+++ app/src/main/java/com/ricardogarfe/rsu/app/MainActivityFragment.java
@@ -1,10 +1,15 @@
 package com.ricardogarfe.rsu.app;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,25 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> rsuLocationResults = new ArrayList<>();
+        rsuLocationResults.add("CONTENEDORES DE PILAS - MERCADO ROJAS CLEMENTE");
+        rsuLocationResults.add("CONTENEDORES DE PILAS - MERCADO CENTRAL");
+        rsuLocationResults.add("CONTENEDORES DE PILAS - COLEGIO JESUS-MARIA");
+
+        ArrayAdapter<String> RSULocationAdapter =
+                new ArrayAdapter<>(
+                        getActivity(),
+                        R.layout.list_item_rsu,
+                        R.id.list_item_rsu_textview,
+                        rsuLocationResults);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_rsu);
+
+        listView.setAdapter(RSULocationAdapter);
+
+        return rootView;
     }
 }
